8ddc306eb8b4c86b1c5a27974cb57846b36dd492
OmPrakash23 <mishraomm5000@gmail.com>
2026-06-28 21:07:52 +0530

    Implemeneted the json parse with the ts assertions

--- Project/dist/06_Assertions.js
+++ Project/dist/06_Assertions.js
@@ -1,1 +1,17 @@
 "use strict";
+const raw = '{ id: 1, name: "Omm" }';
+const riskyUser = JSON.parse(raw);
+console.log(riskyUser.name);
+function isUser(v) {
+    return (typeof v === "object" &&
+        v !== null &&
+        "id" in v &&
+        typeof v.id === "number" &&
+        "name" in v &&
+        typeof v.name === "string");
+}
+// ☝🏻 alternate way
+const maybe = JSON.parse(raw);
+if (isUser(maybe)) {
+    console.log(maybe.name); // safe
+}
