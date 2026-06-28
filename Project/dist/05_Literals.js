253d9a12aeafd5549518eef75892b0d09db9e56b
OmPrakash23 <mishraomm5000@gmail.com>
2026-06-28 20:51:32 +0530

    Completed the literals and implemeted the types in ts

---
+++ Project/dist/05_Literals.js
@@ -0,0 +1,11 @@
+"use strict";
+function move(d) {
+    console.log(d);
+}
+const d1 = "left"; // as const will remain same throughout prog so it will not raise errors
+move(d1);
+// let d2 = "left";
+// move(d2); // this will raise error as thr value of lrt may change anytime in the prog!
+// ☝🏻 issue fix
+let d3 = "left";
+move(d3);
