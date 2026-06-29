b783304af14086e84faa4e0ee778f8636c5db26b
OmPrakash23 <mishraomm5000@gmail.com>
2026-06-29 20:12:31 +0530

    Completed the Unions part and started the intersection part focussed on the array unions and the array intersctions

--- Project/dist/08_Unions.js
+++ Project/dist/08_Unions.js
@@ -1,5 +1,6 @@
 "use strict";
 // Unions - A union in TypeScript is a type that allows a value to be one of several different types, represented using the pipe (|) symbol (e.g., string | number).
+// 1>
 function printId(id) {
     if (typeof id === "string") {
         console.log(id.toLowerCase());
@@ -8,3 +9,23 @@ function printId(id) {
         console.log(id.toFixed(2));
     }
 }
+function describeUser(u) {
+    if (u.role === "Admin") {
+        console.log(u.permissions);
+    }
+    else {
+        console.log(u.loyalityPoints);
+    }
+}
+function describeUserWithInOperator(u) {
+    if ("permissions" in u) {
+        console.log(u.role, "Admin User");
+    }
+    else {
+        console.log(u.loyalityPoints);
+    }
+}
+// Array of the unions and the union of the arrays
+const arrOfUnions = ["a", 1, "b", 2];
+const unionOfArrays = Math.random() > 0.1 ? ["x", "3"] : [1, 2];
+// unionOfArrays.push("5"); // this is a error
--- Project/src/08_Unions.ts
+++ Project/src/08_Unions.ts
@@ -1,9 +1,36 @@
 // Unions - A union in TypeScript is a type that allows a value to be one of several different types, represented using the pipe (|) symbol (e.g., string | number).
 
-function printId(id: string | number){
-    if(typeof id === "string"){
-        console.log(id.toLowerCase());
-    }else{
-        console.log(id.toFixed(2));
-    }
+// 1>
+function printId(id: string | number) {
+  if (typeof id === "string") {
+    console.log(id.toLowerCase());
+  } else {
+    console.log(id.toFixed(2));
+  }
 }
+
+// 2>
+// objects union
+type Admin = { role: "Admin"; permissions: string[] };
+type Customer = { role: "Customer"; loyalityPoints: number };
+
+function describeUser(u: Admin | Customer) {
+  if (u.role === "Admin") {
+    console.log(u.permissions);
+  } else {
+    console.log(u.loyalityPoints);
+  }
+}
+
+function describeUserWithInOperator(u: Admin | Customer) {
+  if ("permissions" in u) {
+    console.log(u.role, "Admin User");
+  } else {
+    console.log(u.loyalityPoints);
+  }
+}
+
+// Array of the unions and the union of the arrays
+const arrOfUnions: (string | number)[]= ["a", 1, "b", 2]
+const unionOfArrays: string[] | number[] = Math.random() > 0.1 ? ["x", "3"] : [1, 2]
+// unionOfArrays.push("5"); // this is a error
