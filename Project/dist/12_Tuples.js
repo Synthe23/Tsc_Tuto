e69fe0e23c163ab61127650e3beecfd2ce0fa3d1
OmPrakash23 <mishraomm5000@gmail.com>
2026-06-29 21:31:04 +0530

    Completed the tuples and the read-only tuples

--- Project/dist/12_Tuples.js
+++ Project/dist/12_Tuples.js
@@ -1,2 +1,5 @@
 "use strict";
-// tuples - In TypeScript, a tuple is a specialized array with a fixed length and predefined types for each position. 
+// tuples - In TypeScript, a tuple is a specialized array with a fixed length and predefined / fixed types for each position. 
+const userEntry = ["Omm", 23];
+const r11 = [200];
+const corners = [0, 0];
--- Project/src/12_Tuples.ts
+++ Project/src/12_Tuples.ts
@@ -1,1 +1,9 @@
-// tuples - In TypeScript, a tuple is a specialized array with a fixed length and predefined types for each position. 
+// tuples - In TypeScript, a tuple is a specialized array with a fixed length and predefined / fixed types for each position. 
+
+const userEntry: [string, number] = ["Omm", 23];
+
+type ResponseFlow = [status: number, message?: string];
+
+const r11: ResponseFlow = [200];
+
+const corners: readonly [number, number] = [0, 0];
