09d412e045369aa025bf8063742c22ff2b8eaa69
OmPrakash23 <mishraomm5000@gmail.com>
2026-06-30 21:15:13 +0530

    Learned the return types in the ts

--- Project/dist/16_Return_Types.js
+++ Project/dist/16_Return_Types.js
@@ -1,2 +1,23 @@
-"use strict";
-const doubleFunction;
+// 1>
+// Inference example
+const doubleFunction = (n) => n * 2;
+// 2>
+// explicit return for the exported/public functions
+export function WebTransportBidirectionalStream(s) {
+    return `Hello ${s}`;
+}
+// 3>
+function returnBool(flag) {
+    if (flag) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+console.log(returnBool(true));
+// 4>
+async function loadCountInferred() {
+    return 23;
+}
+loadCountInferred().then((e) => console.log());
--- Project/src/16_Return_Types.ts
+++ Project/src/16_Return_Types.ts
@@ -1,1 +1,29 @@
-const doubleFunction
+// 1>
+// Inference example
+const doubleFunction = (n: number) => n * 2;
+
+
+// 2>
+// explicit return for the exported/public functions
+export function WebTransportBidirectionalStream(s: string): string {
+  return `Hello ${s}`;
+}
+
+
+// 3>
+function returnBool(flag: boolean): number {
+  if (flag) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+console.log(returnBool(true));
+
+
+// 4>
+async function loadCountInferred() {
+  return 23;
+}
+
+loadCountInferred().then((e) => console.log());
