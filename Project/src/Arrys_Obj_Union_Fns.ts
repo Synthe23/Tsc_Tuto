7f056cd7c0bb095b0716c2072a2c1f5ad7170c1a
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-10 09:10:50 +0530

    Solved some array questions

---
+++ Project/src/Arrys_Obj_Union_Fns.ts
@@ -0,0 +1,23 @@
+// Topics covered in this file
+// * Q1–Q2 → Arrays
+// * Q3–Q4 → Objects
+// * Q5–Q6 → Union Types
+// * Q7–Q9 → Functions
+// * Q10 → TypeScript’s type system reasoning
+
+// Q1: 
+// i> What is the inferred type of numbers and names?
+// let numbers = [1, 2, 3, 4];
+// let names = ["Omm", "Rahul", "Amit"];
+
+// ii> Can you later do this?
+// numbers.push(5);
+// numbers.push("six");
+
+// names.push("Priya");
+// names.push(10);
+// Explain why?
+
+
+// Solution:
+
