3f4617e65cc505c3a2335a3c8510160bdbaa698c
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-13 21:24:13 +0530

    Started the generics section

---
+++ Project/dist/23_generics_intro.js
@@ -0,0 +1,21 @@
+"use strict";
+// understand what is type-parameter <T> is
+// how ts infers <T> from your arguments automatically
+// Generics is a function where the types are blanks that you fill it later.
+// <T> is a placeholder
+// TS will try to understand what <T> should be
+function id(x) {
+    return x;
+}
+// infers T
+// id(5) -> T is a number
+// id('string') -> T is a string
+// id <number>(5)
+const xyzz = id(5);
+console.log(xyzz + 1); // o/t is 6
+console.log(xyzz + 1, id('Syn_23')); // o/t is 6 Syn_23
+console.log(xyzz + 1, id(['Syn_31'])); // o/t is 6 [ 'Syn_31' ]
+function firstGen(arr) {
+    return arr[0];
+}
+console.log(firstGen([1, 2, 3])); // o/t - 1
