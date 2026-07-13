7e438613f6a73dc8732cb9c0b90d0a680a14fe70
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-13 20:43:30 +0530

    Completed the nullish section

--- Project/dist/22_In_Optional_Nullish.js
+++ Project/dist/22_In_Optional_Nullish.js
@@ -6,3 +6,6 @@ function describeUserExample(u) {
     return `User ${u.expiresAt.toISOString()}`;
 }
 console.log(describeUserExample({ role: "Admin", permissions: ["read"] }));
+const P1N3 = { name: 'John' };
+const P2N3 = { name: 'Ben', contact: { email: 'ben@gmail.com' } };
+const email1N3 = P1N3.contact?.email;
--- Project/src/22_In_Optional_Nullish.ts
+++ Project/src/22_In_Optional_Nullish.ts
@@ -9,3 +9,16 @@ function describeUserExample(u: UserExample) {
   return `User ${u.expiresAt.toISOString()}`;
 }
 console.log(describeUserExample({ role: "Admin", permissions: ["read"] }));
+
+// To avoid the runtime crashes
+// ?? and || 
+// obj?.a
+type profileN3 = {
+    name : string;
+    contact? : {email? : string}
+}
+
+const P1N3: profileN3 = {name: 'John'}
+const P2N3: profileN3 = {name: 'Ben', contact: {email: 'ben@gmail.com'}}
+
+const email1N3 = P1N3.contact?.email
