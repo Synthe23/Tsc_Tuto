83c4f59cd3dc6915d73cd81ef6a731f8f52246cf
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-15 20:34:11 +0530

    Completed the helper function and the constructor section

--- Project/dist/27_Util_Function_Union_Helpers.js
+++ Project/dist/27_Util_Function_Union_Helpers.js
@@ -2,7 +2,7 @@
 // ReturnType<F>
 // Parameters<F>
 // InstanceType<Ctor>
-//ConstructorParameters<C>
+// ConstructorParameters<C>
 function ExtractUserInfo(id, isExtractInfo = false) {
     return {
         id,
@@ -12,3 +12,17 @@ function ExtractUserInfo(id, isExtractInfo = false) {
 }
 const argsInfo = ["u1", true];
 const resultInfo = ExtractUserInfo(...argsInfo);
+class PersonN1 {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        return `Hi I am ${this.name}`;
+    }
+}
+const resultInfo1 = ["Omm", 23];
+const abc = new PersonN1(...resultInfo1);
+console.log(abc.greet());
--- Project/src/27_Util_Function_Union_Helpers.ts
+++ Project/src/27_Util_Function_Union_Helpers.ts
@@ -1,7 +1,7 @@
 // ReturnType<F>
 // Parameters<F>
 // InstanceType<Ctor>
-//ConstructorParameters<C>
+// ConstructorParameters<C>
 
 function ExtractUserInfo(id: string, isExtractInfo = false) {
   return {
@@ -16,3 +16,22 @@ type getUserParamsInfo = Parameters<typeof ExtractUserInfo>;
 
 const argsInfo: getUserParamsInfo = ["u1", true];
 const resultInfo: GetUserReturnInfo = ExtractUserInfo(...argsInfo);
+
+class PersonN1{
+    constructor(public name: string, public age: number){
+
+    }
+    greet(){
+        return `Hi I am ${this.name}`
+    }
+}
+
+type PersonInstanceN1 = InstanceType<typeof PersonN1>
+type PersonCtorArgsN1 = ConstructorParameters<typeof PersonN1>
+
+const resultInfo1: PersonCtorArgsN1 = ["Omm", 23]
+const abc: PersonInstanceN1 = new PersonN1(...resultInfo1)
+
+console.log(abc.greet());
+
+
